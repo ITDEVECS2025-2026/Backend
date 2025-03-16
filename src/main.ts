f394495f41d67ba4e316625979d48be3536266e1
Mohammad Arfan <32129450+marfanr@users.noreply.github.com>
2025-03-16 15:14:30 +0700

    feat: add CORS needed by swagger

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,18 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  });
+
   const config = new DocumentBuilder()
   .setTitle('ECS API')  
   .setVersion('1.0')  
   .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, documentFactory);
-  
+
   await app.listen(3000);
 }
 bootstrap();
