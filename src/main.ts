924ab825d6365246875e61afe9aae54d4bbd7c83
Mohammad Arfan <32129450+marfanr@users.noreply.github.com>
2025-03-16 15:22:58 +0700

    feat: serve static file needed by swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { join } from 'path';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +11,9 @@ async function bootstrap() {
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
   });
+
+  app.use('/docs', express.static(join(__dirname, '../node_modules/swagger-ui-dist')));
+
 
   const config = new DocumentBuilder()
   .setTitle('ECS API')  
