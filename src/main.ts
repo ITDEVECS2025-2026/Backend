ab47a25b05b922b80c2104c0a88bf719fa20a7df
Mohammad Arfan <32129450+marfanr@users.noreply.github.com>
2025-03-16 15:29:01 +0700

    fix: fix swagger only displaying a blank page on vercel

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { join } from 'path';
-import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,15 +10,13 @@ async function bootstrap() {
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
   });
 
-  app.use('/docs', express.static(join(__dirname, '../node_modules/swagger-ui-dist')));
-
 
   const config = new DocumentBuilder()
   .setTitle('ECS API')  
   .setVersion('1.0')  
   .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, documentFactory);
+  SwaggerModule.setup('/docs', app, documentFactory);
 
   await app.listen(3000);
 }
