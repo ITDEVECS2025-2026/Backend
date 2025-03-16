01f79aec2417c2475fd55e4016cb3628088373b9
Mohammad Arfan <32129450+marfanr@users.noreply.github.com>
2025-03-16 16:09:45 +0700

    fix: swagger only displaying a blank page on vercel

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PeminjamanModule } from './modules/peminjaman/peminjaman.module';
 import { PrismaModule } from './modules/prisma/prisma.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [PeminjamanModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
+  
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -10,13 +10,25 @@ async function bootstrap() {
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
   });
 
-
   const config = new DocumentBuilder()
   .setTitle('ECS API')  
-  .setVersion('1.0')  
+  .setVersion('1.0')    
   .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('/docs', app, documentFactory);
+  SwaggerModule.setup('/docs', app, documentFactory, {
+    customSiteTitle: "Api Docs",
+    customfavIcon: "https://avatars.githubusercontent.com/u/6936373?s=200&v=4",
+    customJs: [
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js",
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js",
+    ],
+    customCssUrl: [
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css",
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css",
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css",
+    ],
+  });
+
 
   await app.listen(3000);
 }
