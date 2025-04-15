6c80af1a5f6a2e300e16e7506453b0a82191a305
Fadhillahusein <rifqifadhillahhusein@gmail.com>
2025-04-15 16:43:28 +0700

    feat: add new registerAPI

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PeminjamanModule } from './modules/peminjaman/peminjaman.module';
 import { PrismaModule } from './modules/prisma/prisma.module';
 import { DemoZodModule } from './modules/demo-zod/demo-zod.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [PeminjamanModule, PrismaModule, DemoZodModule],
+  imports: [PeminjamanModule, PrismaModule, AuthModule, DemoZodModule],
   controllers: [AppController],
   providers: [AppService],
 
