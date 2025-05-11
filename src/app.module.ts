0797aae6edc9176f3b2f750349ce7c95e4fc981b
Mohammad Arfan <32129450+marfanr@users.noreply.github.com>
2025-05-11 19:55:48 +0700

    refactor: remove User module and related validation services

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PeminjamanModule } from './modules/peminjaman/peminjaman.module';
 import { PrismaModule } from './modules/prisma/prisma.module';
-import { UserModule } from './user/user.module';
 import { DemoZodModule } from './modules/demo-zod/demo-zod.module';
 import { AuthModule } from './modules/auth/auth.module';  
 import { ThreedprintModule } from './modules/threedprint/threedprint.module';
 
 @Module({
-  imports: [PeminjamanModule, PrismaModule, ThreedprintModule, DemoZodModule, AuthModule, UserModule],
+  imports: [PeminjamanModule, PrismaModule, ThreedprintModule, DemoZodModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 
